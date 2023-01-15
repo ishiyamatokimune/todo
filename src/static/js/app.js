f86f55ccd55aee845de4813ab420f3f82a8782bb
ishiyamatokimune <ishiyama.tokimune.io3@is.naist.jp>
2023-01-15 23:12:10 +0900

    Fix header tag

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,7 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
-            <H1>ToDo App by Tokimune Ishiyama</H1>
+            ToDo App by Tokimune Ishiyama
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
